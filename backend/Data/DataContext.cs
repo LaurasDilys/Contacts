abeb7dc8032cc384ef91e30a62bbfd50878fb3a4
Lauras <lauras.dilys@gmail.com>
2021-10-19 02:20:27 +0300

    added user entity

---
+++ backend/Data/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Data
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        // DbSets
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var user = new User
+            {
+                SecurityStamp = Guid.NewGuid().ToString(),
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Lauras",
+                FirstName = "Lauras",
+                LastName = "Dilys"
+            };
+
+            modelBuilder.Entity<User>().HasData(user);
+        }
+    }
+}
