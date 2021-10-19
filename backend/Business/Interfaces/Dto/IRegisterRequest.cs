3c857874b0d0109ef0c0ef05a294106425bf9a78
Lauras <lauras.dilys@gmail.com>
2021-10-19 04:08:35 +0300

    added interfaces

--- backend/Business/Interfaces/Dto/IRegisterRequest.cs
+++ backend/Business/Interfaces/Dto/IRegisterRequest.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Business.Interfaces.Dto
 {
-    public interface ILoginRequest
+    public interface IRegisterRequest
     {
         public string UserName { get; set; }
         public string Password { get; set; }
-        public bool Remember { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
