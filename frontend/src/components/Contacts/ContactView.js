8202cfb37217f1007832ca59dc7a907ae0ededb7
Lauras <lauras.dilys@gmail.com>
2021-10-21 16:51:35 +0300

    almost finished ContactView

--- frontend/src/components/Contacts/ContactView.js
+++ frontend/src/components/Contacts/ContactView.js
@@ -1,5 +1,7 @@
 import './Contacts.css'
 import { Avatar, Button, Divider, TextField } from "@mui/material";
+import PhoneInput from 'react-phone-input-2';
+import { useEffect, useRef, useState } from 'react';
 
 const stringToColor = string => {
   let hash = 0;
@@ -33,6 +35,15 @@ const initialsAvatar = (firstName, lastName) => {
 }
 
 const ContactView = ({ contact, setEditing }) => {
+  const [formattedNumber, setFormattedNumber] = useState();
+  const phoneInputRef = useRef(null);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFormattedNumber(phoneInputRef?.current?.state.formattedNumber);
+    }, 1); // it takes some time for PhoneInput to calculate formattedNumber
+    return () => clearTimeout(timeout);
+  }, [contact])
 
   return (
     <div>
@@ -42,20 +53,61 @@ const ContactView = ({ contact, setEditing }) => {
       </div>
 
       <div className='flex-row'>
-        <Avatar {...initialsAvatar(contact.firstName, contact.lastName)} />
-        <h1>{contact.firstName} {contact.lastName}</h1>
+        <div className='avatar-div-row'>
+          <div className='avatar-div-col'>
+            <Avatar {...initialsAvatar(contact.firstName, contact.lastName)} />
+          </div>
+        </div>
+        <div className='name-div'>
+          <h1>{contact.firstName} {contact.lastName}</h1>
+        </div>
       </div>
 
+      {contact.phoneNumber?.length > 0 &&
       <div className='flex-row'>
-        <p>Phone Number</p>
-        <TextField
-          defaultValue={contact.phoneNumber}
-          variant='standard'
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-      </div>
+
+        <div className='contact-entry-description'>
+          <span>Phone Number</span>
+        </div>
+
+        <div className='contact-entry'>
+          <a href={`tel: +${contact.phoneNumber}`}>
+            {formattedNumber}
+          </a>
+          <PhoneInput
+            ref={phoneInputRef}
+            style={{ display: 'none' }}
+            value={contact.phoneNumber}
+            masks={{lt: '(...) .....'}}
+          />
+        </div>
+      </div>}
+
+      {contact.email?.length > 0 &&
+      <div className='flex-row'>
+
+        <div className='contact-entry-description'>
+          <span>Email</span>
+        </div>
+
+        <div className='contact-entry'>
+          <a href={`mailto: ${contact.email}`}>
+            {contact.email}
+          </a>
+        </div>
+      </div>}
+
+      {contact.notes?.length > 0 &&
+      <div className='flex-row'>
+
+        <div className='contact-entry-description'>
+          <span>Notes</span>
+        </div>
+
+        <div className='contact-entry'>
+          <span>{contact.notes}</span>
+        </div>
+      </div>}
 
       {/* phoneNumber: '37061417706',
   email: 'lauras.dilys@gmail.com',
