3055f568629319e8e5d5c5f9a959854c8fab5464
Lauras <lauras.dilys@gmail.com>
2021-10-21 22:50:42 +0300

    added buttons and all of the fields

--- frontend/src/components/Contacts/ContactView.js
+++ frontend/src/components/Contacts/ContactView.js
@@ -1,5 +1,6 @@
 import './Contacts.css'
 import { Avatar, Button, Divider, TextField } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 import PhoneInput from 'react-phone-input-2';
 import { useEffect, useRef, useState } from 'react';
 import Description from './Description';
@@ -40,11 +41,14 @@ const initialsAvatar = (firstName, lastName) => {
 
 const ContactView = ({ contact, setEditing }) => {
   const [formattedNumber, setFormattedNumber] = useState();
+  const [formattedAltNumber, setFormattedAltNumber] = useState();
   const phoneInputRef = useRef(null);
+  const altPhoneInputRef = useRef(null);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setFormattedNumber(phoneInputRef?.current?.state.formattedNumber);
+      setFormattedAltNumber(altPhoneInputRef?.current?.state.formattedNumber);
     }, 1); // it takes some time for PhoneInput to calculate formattedNumber
     return () => clearTimeout(timeout);
   }, [contact])
@@ -52,7 +56,20 @@ const ContactView = ({ contact, setEditing }) => {
   return (
     <div>
       <div className='contact-area-top'>
-        <Button onClick={() => setEditing(true)}>Edit</Button>
+        {/* // */}
+        <Button>
+          <AddIcon />
+          <span className='button-span'>New</span>
+        </Button>
+        {/* // */}
+        <Button onClick={() => setEditing(true)}>
+          <span className='button-span'>Edit</span>
+        </Button>
+        {/* // */}
+        <Button>
+          <span className='button-span'>Share</span>
+        </Button>
+        {/* // */}
         <Divider />
       </div>
 
@@ -83,6 +100,22 @@ const ContactView = ({ contact, setEditing }) => {
         </div>
       </div>}
 
+      {contact.alternativePhoneNumber?.length > 0 &&
+      <div className='flex-row'>
+        <Description>Alternative Phone Number</Description>
+        <div className='contact-entry'>
+          <a href={`tel:+${contact.alternativePhoneNumber}`}>
+            {formattedAltNumber}
+          </a>
+          <PhoneInput
+            ref={altPhoneInputRef}
+            style={{ display: 'none' }}
+            value={contact.alternativePhoneNumber}
+            masks={{lt: '(...) .....'}}
+          />
+        </div>
+      </div>}
+
       {contact.email?.length > 0 &&
       <div className='flex-row'>
         <Description>Email</Description>
@@ -90,6 +123,24 @@ const ContactView = ({ contact, setEditing }) => {
           <a href={`mailto:${contact.email}`}>
             {contact.email}
           </a>
+        </div>
+      </div>}
+
+      {contact.alternativeEmail?.length > 0 &&
+      <div className='flex-row'>
+        <Description>Alternative Email</Description>
+        <div className='contact-entry'>
+          <a href={`mailto:${contact.alternativeEmail}`}>
+            {contact.alternativeEmail}
+          </a>
+        </div>
+      </div>}
+
+      {contact.dateOfBirth?.length > 0 &&
+      <div className='flex-row'>
+        <Description>Date Of Birth</Description>
+        <div className='contact-entry'>
+          <span>{contact.dateOfBirth}</span>
         </div>
       </div>}
 
