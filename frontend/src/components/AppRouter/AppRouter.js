e8619e6e12648905445cb5e1cf2cab308264e37c
Lauras <lauras.dilys@gmail.com>
2021-10-23 05:12:08 +0300

    fixed "Router not working with history prop issue" by downgrading to react-router-dom version 5.2.0

--- frontend/src/components/AppRouter/AppRouter.js
+++ frontend/src/components/AppRouter/AppRouter.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import TopNav from './TopNav';
 import RouteSwitch from './RouteSwitch';
