3bfdf2244f8a67003c8e990c95faed026ca81ab0
Lauras <lauras.dilys@gmail.com>
2021-10-24 05:02:04 +0300

    nav position now fixed

--- frontend/src/components/AppRouter/AppRouter.js
+++ frontend/src/components/AppRouter/AppRouter.js
@@ -6,10 +6,12 @@ import RouteSwitch from './RouteSwitch';
 import { useSelector } from 'react-redux';
 import { userState } from '../../state/selectors';
 import Routes from './Routes';
+import { Toolbar } from '@mui/material';
 
 export const history = createBrowserHistory();
 
 const AppRouter = () => {
+  const appBarHeight = 48;
   const { loggedIn } = useSelector(userState);
   const [availableRoutes, setAvailableRoutes] = useState([]);
 
@@ -20,8 +22,13 @@ const AppRouter = () => {
   }, [loggedIn])
 
   return (
-    <Router history={history}>
-      <TopNav routes={availableRoutes} />
+    <Router history={history} style={{ overflowY: 'visible' }}>
+      <div style={{ height: appBarHeight }} />
+      <TopNav // empty div above corrects for nav height
+        position='fixed' // nav stays fixed when x axis overflows
+        height={appBarHeight} // making sure that nav and div above have the same height
+        routes={availableRoutes}
+      />
       {/* <Notification /> */}
       <RouteSwitch routes={availableRoutes} loggedIn={loggedIn} />
     </Router>
