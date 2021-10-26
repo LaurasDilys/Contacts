4bf8bc60f8add32b9007c20f66e4f12448e415ca
Lauras <lauras.dilys@gmail.com>
2021-10-27 02:01:52 +0300

    small correction

--- backend/Application/Dto/Contact/ContactsResponse.cs
+++ backend/Application/Dto/Contact/ContactsResponse.cs
@@ -4,9 +4,9 @@ namespace Application.Dto.Contact
 {
     public class ContactsResponse
     {
-        public ICollection<ContactResponse> MyContacts { get; set; }
-        public ICollection<ContactResponse> Received { get; set; }
+        public ICollection<ContactResponse> Other { get; set; } // All created contacts that aren't shared with anyone
         public ICollection<ContactResponse> Shared { get; set; }
+        public ICollection<ContactResponse> Received { get; set; }
         public ICollection<ContactResponse> Unaccepted { get; set; }
     }
 }
