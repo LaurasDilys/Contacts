e2a09b4a5b76ba7d4ffce27fa3d13513265ec459
Lauras <lauras.dilys@gmail.com>
2021-10-27 01:10:23 +0300

    testing many-to-many ContactUser relationship

--- backend/Api/Controllers/ContactsController.cs
+++ backend/Api/Controllers/ContactsController.cs
@@ -1,12 +1,17 @@
 ﻿using Application.Dto.Contact;
 using Application.Services;
+using Data;
 using Data.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Api.Controllers
@@ -18,11 +23,13 @@ namespace Api.Controllers
     {
         private readonly ContactsService _contactsService;
         private readonly UserManager<User> _userManager;
+        private readonly DataContext _context;
 
-        public ContactsController(ContactsService contactsService, UserManager<User> userManager)
+        public ContactsController(ContactsService contactsService, UserManager<User> userManager, DataContext context)
         {
             _contactsService = contactsService;
             _userManager = userManager;
+            _context = context;
         }
 
         [HttpGet("User/{userKey}/Contacts", Name = nameof(Get))]
@@ -68,5 +75,177 @@ namespace Api.Controllers
 
             return Ok();
         }
+
+        //
+        //
+        //
+
+        [AllowAnonymous]
+        [HttpPatch(nameof(Share))]
+        public IActionResult Share()
+        {
+            //var laurasShared = new ContactUser
+            //{
+            //    ContactId = "94702cc7-3c19-49d1-b396-60a17f04d9ab",
+            //    UserId = "6b675e09-54a7-4c4a-9296-c1ff08f99c91"
+            //};
+
+            var sharedShared = new ContactUser
+            {
+                ContactId = "aaa8d67e-f562-41d8-8d4a-e6367a542dc1",
+                UserId = "020619ed-d5b0-4465-9aa3-b7b6651cfa69"
+            };
+
+            //_context.ContactUsers.Add(laurasShared);
+            //_context.Users.Include(u => u.UnacceptedShares)
+            //    .FirstOrDefault(u => u.Id == "6b675e09-54a7-4c4a-9296-c1ff08f99c91")
+            //    .UnacceptedShares.Add(new UnacceptedShare
+            //{
+            //    Id = Guid.NewGuid().ToString(),
+            //    ContactId = "94702cc7-3c19-49d1-b396-60a17f04d9ab"
+            //});
+
+            _context.ContactUsers.Add(sharedShared);
+            _context.Users.Include(u => u.UnacceptedShares)
+                .FirstOrDefault(u => u.Id == "020619ed-d5b0-4465-9aa3-b7b6651cfa69")
+                .UnacceptedShares.Add(new UnacceptedShare
+            {
+                Id = Guid.NewGuid().ToString(),
+                ContactId = "aaa8d67e-f562-41d8-8d4a-e6367a542dc1"
+            });
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpPatch(nameof(Accept))]
+        public IActionResult Accept()
+        {
+            AcceptOrDecline();
+
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpPatch(nameof(GetAll))]
+        public IActionResult GetAll()
+        {
+            var allContacts = new List<C>();
+
+            var lauras = _context.Users
+                .Include(u => u.Contacts)
+                .Include(u => u.UnacceptedShares)
+                .Include(u => u.ContactUsers).ThenInclude(cu => cu.Contact)
+                .FirstOrDefault(u => u.Id == "020619ed-d5b0-4465-9aa3-b7b6651cfa69");
+
+            var contacts = lauras.Contacts;
+            foreach (var c in contacts)
+            {
+                allContacts.Add(new C
+                {
+                    FirstName = c.FirstName,
+                    LastName = c.LastName
+                });
+            }
+
+            if (lauras.ShowMyContact)
+            {
+                allContacts.Add(new C
+                {
+                    FirstName = lauras.FirstName,
+                    LastName = "(me)"
+                });
+            }
+
+            var received = lauras.ContactUsers.Select(cu => cu.Contact);
+            var unaccepted = lauras.UnacceptedShares.Select(us => us.ContactId);
+            if (unaccepted.Count() > 0)
+            {
+                foreach (var c in received)
+                {
+                    if (unaccepted.Any(id => id == c.Id))
+                    {
+                        allContacts.Add(new C
+                        {
+                            FirstName = c.FirstName,
+                            LastName = "(unaccepted)"
+                        });
+                    }
+                    else
+                    {
+                        allContacts.Add(new C
+                        {
+                            FirstName = c.FirstName,
+                            LastName = c.LastName
+                        });
+                    }
+                }
+            }
+            else
+            {
+                foreach (var c in received)
+                {
+                    allContacts.Add(new C
+                    {
+                        FirstName = c.FirstName,
+                        LastName = c.LastName
+                    });
+                }
+            }
+
+            return Ok(allContacts);
+        }
+
+        [AllowAnonymous]
+        [HttpPatch(nameof(RemoveShared))]
+        public IActionResult RemoveShared()
+        {
+            var contactUser = _context.ContactUsers
+                .FirstOrDefault(cu => cu.ContactId == "aaa8d67e-f562-41d8-8d4a-e6367a542dc1"
+                                      && cu.UserId == "020619ed-d5b0-4465-9aa3-b7b6651cfa69");
+
+            _context.ContactUsers.Remove(contactUser);
+            AcceptOrDecline();
+
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpPatch(nameof(ToggleMyContact))]
+        public IActionResult ToggleMyContact()
+        {
+            var lauras = _context.Users
+                .Include(u => u.UnacceptedShares)
+                .FirstOrDefault(u => u.Id == "020619ed-d5b0-4465-9aa3-b7b6651cfa69");
+
+            var current = lauras.ShowMyContact;
+            lauras.ShowMyContact = !current;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        private void AcceptOrDecline()
+        {
+            var lauras = _context.Users
+                .Include(u => u.UnacceptedShares)
+                .FirstOrDefault(u => u.Id == "020619ed-d5b0-4465-9aa3-b7b6651cfa69");
+
+            if (lauras != null)
+            {
+                var toBeAccepted = lauras.UnacceptedShares.FirstOrDefault(us => us.ContactId == "aaa8d67e-f562-41d8-8d4a-e6367a542dc1");
+                lauras.UnacceptedShares.Remove(toBeAccepted);
+                _context.SaveChanges();
+            }
+        }
     }
+
+    public class C
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
 }
