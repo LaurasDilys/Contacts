a0433ff2d99185a1e72911c921e6bfa43327b352
Lauras <lauras.dilys@gmail.com>
2021-10-26 16:48:07 +0300

    Db is fully configured (apart from migration)

--- backend/Data/Models/Contact.cs
+++ backend/Data/Models/Contact.cs
@@ -1,26 +1,44 @@
-﻿using Business.Interfaces.Models;
-using System;
+﻿using Business.Interfaces;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Data.Models
 {
-    public class Contact : IContact
+    public class Contact : IContactInformation
     {
+        [Key]
         public string Id { get; set; }
+
+        [MaxLength(50)]
         public string FirstName { get; set; }
+
+        [MaxLength(50)]
         public string LastName { get; set; }
+
+        [MaxLength(50)]
         public string PhoneNumber { get; set; }
+
+        [MaxLength(50)]
         public string AlternativePhoneNumber { get; set; }
+
+        [MaxLength(50)]
         public string Email { get; set; }
+
+        [MaxLength(50)]
         public string AlternativeEmail { get; set; }
+
+        [MaxLength(50)]
+        public string Address { get; set; }
+
+        [MaxLength(50)]
         public string DateOfBirth { get; set; }
+
+        [MaxLength(50)]
         public string Notes { get; set; }
 
         public string CreatorId { get; set; }
         public User Creator { get; set; }
+
+        public ICollection<ContactUser> ContactUsers { get; set; }
     }
 }
---
+++ backend/Data/Models/UnacceptedShare.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models
+{
+    public class UnacceptedShare
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public string ContactId { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
