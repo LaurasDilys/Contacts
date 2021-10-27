41cf3fd2bee54a247a10b9e5fcdf5a8be787c98a
Lauras <lauras.dilys@gmail.com>
2021-10-27 15:03:31 +0300

    using ContactTypes in ContactResponse

---
+++ backend/Application/Models/ContactTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class ContactTypes
+    {
+        public const string Me = "ME"; // Contact information of the user, who's requesting their contacts
+        public const string Other = "OTHER"; // User's created contacts that aren't shared with anyone
+        public const string Shared = "SHARED";
+        public const string Received = "RECEIVED"; // Received and accepted
+        public const string Unaccepted = "UNACCEPTED"; // Received, but not accepted
+    }
+}
