749dc43c204589f1a6d8ce6d2f18a90b95bfb7d1
Lauras <lauras.dilys@gmail.com>
2021-10-27 18:18:02 +0300

    UserResponse doesn't need all of contact information

--- backend/Application/Dto/User/UserResponse.cs
+++ backend/Application/Dto/User/UserResponse.cs
@@ -2,9 +2,11 @@
 
 namespace Application.Dto.User
 {
-    public class UserResponse : ContactInformation
+    public class UserResponse
     {
         public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string UserName { get; set; }
         public bool ShowMyContact { get; set; }
     }
