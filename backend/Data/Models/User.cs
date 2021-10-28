e14a0925e51130e003451995e4db3fe4bc0ae026
Lauras <lauras.dilys@gmail.com>
2021-10-28 13:11:11 +0300

    first and last names required

--- backend/Data/Models/User.cs
+++ backend/Data/Models/User.cs
@@ -7,9 +7,11 @@ namespace Data.Models
 {
     public class User : IdentityUser, IContactInformation
     {
+        [Required]
         [MaxLength(50)]
         public string FirstName { get; set; }
 
+        [Required]
         [MaxLength(50)]
         public string LastName { get; set; }
 
