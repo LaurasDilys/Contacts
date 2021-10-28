fadf0d01d3bf8e8d3692a178008389f4931de8e8
Lauras <lauras.dilys@gmail.com>
2021-10-28 13:13:55 +0300

    broadened user responses

---
+++ backend/Application/Dto/User/UpdateUserRequest.cs
@@ -0,0 +1,9 @@
+﻿using Business.Models;
+
+namespace Application.Dto.User
+{
+    public class UpdateUserRequest : ContactInformation
+    {
+        public string Id { get; set; }
+    }
+}
--- backend/Application/Dto/User/UserResponse.cs
+++ backend/Application/Dto/User/UserResponse.cs
@@ -2,11 +2,9 @@
 
 namespace Application.Dto.User
 {
-    public class UserResponse
+    public class UserResponse : ContactInformation
     {
         public string Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string UserName { get; set; }
         public bool ShowMyContact { get; set; }
     }
