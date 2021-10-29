4df74645b72f9c67c42fd4198dff10e40f9eaa68
Lauras <lauras.dilys@gmail.com>
2021-10-29 15:16:29 +0300

    implementing ContactsProvider

--- frontend/src/components/ContactsProviders/ContactsProvider.js
+++ frontend/src/components/ContactsProviders/ContactsProvider.js
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { contactsState } from '../../state/selectors';
 import Contacts from '../Contacts/Contacts';
+import { ALL, SHARED, RECEIVED, UNACCEPTED } from '../../domain/contactTypes';
 
 const ContactsProvider = () => {
-  const { contacts: allContacts } = useSelector(contactsState);
+  const { contacts: allContacts, selectedContacts } = useSelector(contactsState);
+  const [filteredContacts, setFilteredContacts] = useState([]);
+
+  const filterProvidedContacts = () => {
+    if (selectedContacts === ALL) {
+      setFilteredContacts(allContacts);
+    } else {
+      setFilteredContacts(allContacts.filter(c => c.type === selectedContacts));
+    }
+  }
+
+  useEffect(() => {
+    filterProvidedContacts();
+  }, [allContacts])
+
+  useEffect(() => {
+    allContacts.forEach(c => c.selected = false);
+    filterProvidedContacts();
+  }, [selectedContacts])
 
   return (
-    <Contacts providedContacts={allContacts} />
+    <Contacts providedContacts={filteredContacts} />
   );
 }
 
