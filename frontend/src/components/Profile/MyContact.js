54be36582b4cdd64620d8a20c18cf14526dad8fa
Lauras <lauras.dilys@gmail.com>
2021-10-31 03:29:02 +0200

    using useSize for better scroll area rendering

--- frontend/src/components/Profile/MyContact.js
+++ frontend/src/components/Profile/MyContact.js
@@ -1,19 +1,19 @@
-import { Button, Divider, FormControlLabel } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { userState } from '../../state/selectors';
-import Details from '../Contacts/Details';
-// import './Profile.css';
 import { styled } from '@mui/material/styles';
 import Switch from '@mui/material/Switch';
 import MyContactView from './MyContactView';
 import MyContactEdit from './MyContactEdit';
+import { useSize } from '../Contacts/Contacts';
 
 const MyContact = () => {
   const { user } = useSelector(userState);
   const [editing, setEditing] = useState(false);
   const [scrollAreaHeight, setScrollAreaHeight] = useState();
   const [scrollBarWidth, setScrollBarWidth] = useState(0);
+  const myContactRef = useRef();
+  const myContactAreaSize = useSize(myContactRef);
 
   const handleResize = () => {
     if (scrollBarWidth === 0 && window.innerWidth - document.documentElement.clientWidth > 0) {
@@ -36,7 +36,7 @@ const MyContact = () => {
   return (
     user === null ?
     null :
-    <div className='flex-row center'>
+    <div ref={myContactRef} className='flex-row center'>
       {editing ?
       <MyContactEdit setEditing={setEditing} scrollAreaHeight={scrollAreaHeight} /> :
       <MyContactView setEditing={setEditing} scrollAreaHeight={scrollAreaHeight} />}
@@ -44,47 +44,37 @@ const MyContact = () => {
   );
 }
 
-// const Android12Switch = styled(Switch)(({ theme }) => ({
-//   padding: 8,
-//   '& .MuiSwitch-track': {
-//     borderRadius: 22 / 2,
-//     '&:before, &:after': {
-//       content: '""',
-//       position: 'absolute',
-//       top: '50%',
-//       transform: 'translateY(-50%)',
-//       width: 16,
-//       height: 16,
-//     },
-//     '&:before': {
-//       backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-//         theme.palette.getContrastText(theme.palette.primary.main),
-//       )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
-//       left: 12,
-//     },
-//     '&:after': {
-//       backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-//         theme.palette.getContrastText(theme.palette.primary.main),
-//       )}" d="M19,13H5V11H19V13Z" /></svg>')`,
-//       right: 12,
-//     },
-//   },
-//   '& .MuiSwitch-thumb': {
-//     boxShadow: 'none',
-//     width: 16,
-//     height: 16,
-//     margin: 2,
-//   },
-// }));
-
-// <FormControlLabel
-//   control={
-//     <Android12Switch
-//       checked={showContact}
-//       onChange={handleContactVisiblityChange}
-//     />
-//   }
-//   label="Android 12"
-// />
-
 export default MyContact;
+
+export const MyContactSwitch = styled(Switch)(({ theme }) => ({
+  padding: 8,
+  '& .MuiSwitch-track': {
+    borderRadius: 22 / 2,
+    '&:before, &:after': {
+      content: '""',
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      width: 16,
+      height: 16,
+    },
+    '&:before': {
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+        theme.palette.getContrastText(theme.palette.primary.main),
+      )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
+      left: 12,
+    },
+    '&:after': {
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+        theme.palette.getContrastText(theme.palette.primary.main),
+      )}" d="M19,13H5V11H19V13Z" /></svg>')`,
+      right: 12,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxShadow: 'none',
+    width: 16,
+    height: 16,
+    margin: 2,
+  },
+}));
