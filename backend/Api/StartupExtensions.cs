d396baf052325548ea347ed625a0f75771307dcc
Lauras <lauras.dilys@gmail.com>
2021-11-02 02:28:38 +0200

    added chosen password requirements to IdentityOptions

--- backend/Api/StartupExtensions.cs
+++ backend/Api/StartupExtensions.cs
@@ -74,9 +74,9 @@ namespace Api
             services.Configure<IdentityOptions>(options =>
             {
                 options.Password.RequiredLength = 6;
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
                 options.Password.RequireNonAlphanumeric = false;
             });
         }
